4639031c6353a57be04e759674c7df5daec047ae
Taylor MacDonald <tcmacdonald@gmail.com>
2017-04-25 14:43:23 -0400

    US7925 Proof of Concept Decouple dependencies from global stylesheet rules; update webpack for compiled output

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,9 +1,11 @@
 var webpack = require('webpack');
 var helpers = require('./helpers');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    'app': ['./src/main.js']
+    'app': ['./src/main.js'],
+    'vendor': ['./assets/stylesheets/vendor.scss']
   },
 
   resolve: {
@@ -23,8 +25,11 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loaders: ['raw-loader', 'sass-loader']
+        loader: ExtractTextPlugin.extract(['raw-loader', 'sass-loader'])
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
 };
