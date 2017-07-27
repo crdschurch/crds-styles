914d79921e7b633fd69f48b57d5eba94ed4dd6d7
Taylor MacDonald <tcmacdonald@gmail.com>
2017-07-27 11:11:56 -0400

    US7925 Assets Add support for command line arguments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var sass = require('node-sass');
 var fs = require('fs-extra');
 var pkgJson = require('../package.json');
+var argv = require('yargs').argv;
 
 var CRDS = {};
 
@@ -8,7 +9,10 @@ CRDS.Styles = function() {
   var slug = './dist/crds-styles-' + pkgJson.version;
   this.outputFile = slug + '.css';
   this.minOutputFile = slug + '.min.css';
-  this.build();
+  this.debug = argv.debug || false;
+  if(argv.build) {
+    this.build();
+  }
 };
 
 CRDS.Styles.prototype.bind = function(fn, me) {
@@ -18,6 +22,7 @@ CRDS.Styles.prototype.bind = function(fn, me) {
 };
 
 CRDS.Styles.prototype.build = function() {
+  this.log('build()');
   this.compile('nested');
   setTimeout(function() {
     this.compile('compressed');
@@ -25,6 +30,7 @@ CRDS.Styles.prototype.build = function() {
 }
 
 CRDS.Styles.prototype.compile = function(style) {
+  this.log('compile()');
   var filename = style == 'compressed' ? this.minOutputFile : this.outputFile;
   sass.render({
     file: './assets/stylesheets/bootstrap.scss',
@@ -39,30 +45,42 @@ CRDS.Styles.prototype.compile = function(style) {
 };
 
 CRDS.Styles.prototype.onCompile = function(error, result, filename) {
+  this.log('onCompile()');
   if (error) {
-    console.log(error.status);
-    console.log(error.column);
-    console.log(error.message);
-    console.log(error.line);
+    this.log(error.status);
+    this.log(error.column);
+    this.log(error.message);
+    this.log(error.line);
   } else {
     fs.ensureDir('./dist')
       .then(function() {
-        // console.log(result.stats);
+        // this.log(result.stats);
         this.writeFile(result.css, filename);
         this.writeFile(result.map, filename + '.map');
       }.bind(this))
       .catch(function(err) {
-        console.error(err);
+        this.error(err);
       }.bind(this));
   }
 };
 
 CRDS.Styles.prototype.writeFile = function(payload, outputFile) {
+  this.log('writeFile()');
   fs.writeFile(outputFile, payload, function(err){
     if(!err){
       console.log(outputFile);
     }
-  });
+  }.bind(this));
+}
+
+CRDS.Styles.prototype.log = function(str) {
+  if(this.debug) {
+    console.log(str);
+  }
+}
+
+CRDS.Styles.prototype.error = function(str) {
+  console.error(str);
 }
 
 new CRDS.Styles();
