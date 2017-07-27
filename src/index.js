99baaaeff83c6081c23b22fedeeef09e25aa8142
Taylor MacDonald <tcmacdonald@gmail.com>
2017-07-27 14:32:57 -0400

    US7925 Assets Integrate S3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 var sass = require('node-sass');
 var fs = require('fs-extra');
+var path = require('path');
+var async = require('async');
 var pkgJson = require('../package.json');
 var argv = require('yargs').argv;
+var AWS = require('aws-sdk');
+
+// ------------------------------------------------- |
 
 var CRDS = {};
 
 CRDS.Styles = function() {
+  this.setup();
+  this.init();
+};
+
+CRDS.Styles.prototype.setup = function() {
   var slug = './dist/crds-styles-' + pkgJson.version;
   this.outputFile = slug + '.css';
   this.minOutputFile = slug + '.min.css';
   this.debug = argv.debug || false;
+
+  AWS.config.update({
+    accessKeyId: process.env.CRDS_STYLES_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.CRDS_STYLES_AWS_SECRET_KEY
+  })
+}
+
+CRDS.Styles.prototype.init = function() {
   if(argv.build) {
     this.build();
+  } else if(argv.publish) {
+    this.publish();
   }
-};
+}
 
 CRDS.Styles.prototype.bind = function(fn, me) {
   return function() {
@@ -72,6 +92,39 @@ CRDS.Styles.prototype.writeFile = function(payload, outputFile) {
     }
   }.bind(this));
 }
+
+// ------------------------------------------------- |
+
+CRDS.Styles.prototype.publish = function() {
+  this.log('publish()');
+  this.s3 = new AWS.S3();
+  var directoryPath = './dist';
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      this.log('Cannot read directory ' + directoryPath + ' or doesn\'t exist')
+    } else {
+      async.mapLimit(files, 5, this.upload.bind(this))
+    }
+  }.bind(this));
+}
+
+CRDS.Styles.prototype.upload = function(filename) {
+  var stream = fs.createReadStream('./dist/' + filename);
+  var ext = path.extname(filename);
+  var mime_type = ext == '.css' ? 'text/css' : 'application/octet-stream'
+  var params = {
+    Bucket: process.env.CRDS_STYLES_S3_BUCKET,
+    Key: 'styles/' + filename,
+    ContentType: mime_type,
+    Body: stream,
+    ACL: 'public-read'
+  };
+  this.s3.upload(params, function(err, data) {
+    console.log(err, data);
+  });
+}
+
+// ------------------------------------------------- |
 
 CRDS.Styles.prototype.log = function(str) {
   if(this.debug) {
