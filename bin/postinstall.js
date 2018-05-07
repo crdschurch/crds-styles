9087058a59dcef52e0752f910e4e837ba1e9622a
Taylor MacDonald <tcmacdonald@gmail.com>
2018-05-07 18:03:46 -0400

    DE5387 Post Install Remove the post-install scripts, we're going 3.0 baby

--- bin/postinstall.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-console.log('\x1b[33m%s\x1b[0m', `
- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-| As of v2.5.0, the import statement has changed. Use:        |
-|                                                             |
-|     @import "~crds-styles/assets/stylesheets/crds-styles";  |
- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-`);
